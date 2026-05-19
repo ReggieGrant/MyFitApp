c81666a2b8a10425a1a31cf42622c717f5779047
ReggieGrant <grantreggie09@gmail.com>
2026-05-18 17:32:53 -0700

    Initial Commit

---
+++ MyFitApp/MyFitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFitAppApp.swift
+//  MyFitApp
+//
+//  Created by Reginald Grant on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct MyFitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
